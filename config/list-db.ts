3817bbee91c1822448854ad53274be953a5caf86
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2023-10-07 22:07:36 -0300

    Cambio de db de list

---
+++ config/list-db.ts
@@ -0,0 +1,114 @@
+export const list = [
+    {
+        title: "Street Fighter",
+        img: "/images/games/1.webp",
+        price: "$49.99",
+        description:
+            "Embark on a journey of self-discovery and empowerment in this immersive RPG.",
+    },
+    {
+        title: "Horizon zero dawn",
+        img: "/images/games/2.webp",
+        price: "$39.99",
+        description:
+            "Horizon Zero Dawn es un videojuego de acción, aventura y de mundo abierto desarrollado por Guerrilla Games y distribuido por Sony Interactive Entertainment para PlayStation 4 y Microsoft Windows.",
+    },
+    {
+        title: "Tomb Raider",
+        img: "/images/games/3.webp",
+        price: "$29.99",
+        description:
+            "Tomb Raider es un videojuego de acción-aventura, de mundo abierto desarrollado por Crystal Dynamics y distribuido por Square Enix. Es el décimo título de la serie Tomb Raider y el quinto título desarrollado por Crystal Dynamics.",
+    },
+    {
+        title: "League of Legends",
+        img: "/images/games/4.webp",
+        price: "$59.99",
+        description:
+            "League of Legends, es un videojuego multijugador de arena de batalla en línea desarrollado y publicado por Riot Games. Inspirándose en Defense of the Ancients, un mapa personalizado para Warcraft III, los fundadores de Riot buscaron desarrollar un juego independiente del mismo género.",
+    },
+    {
+        title: "The Last of Us Part II",
+        img: "/images/games/5.webp",
+        price: "$24.99",
+        description:
+            "The Last of Us Part II es un videojuego de terror y de acción y aventuras de 2020 desarrollado por Naughty Dog y publicado por Sony Interactive Entertainment para PlayStation 4.",
+    },
+    {
+        title: "Overwatch 2",
+        img: "/images/games/6.webp",
+        price: "$34.99",
+        description:
+            "Overwatch 2 es un videojuego de disparos en primera persona. Fue desarrollado y publicado por Blizzard Entertainment. Es una secuela del hero shooter de 2016 Overwatch.",
+    },
+    {
+        title: "Mirror's Edge",
+        img: "/images/games/7.webp",
+        price: "$44.99",
+        description:
+            "Mirror's Edge es un videojuego de acción-aventura en primera persona desarrollado por EA Digital Illusions CE (DICE). El juego fue lanzado para PlayStation 3 y Xbox 360 en noviembre del 2008, y para Windows en enero del 2009.",
+    },
+    {
+        title: "Fortnite",
+        img: "/images/games/8.webp",
+        price: "$29.99",
+        description:
+            "Fortnite es un videojuego del año 2017 desarrollado por la empresa Epic Games lanzado como diferentes paquetes de software que presentan diferentes modos de juego, pero que comparten el mismo motor de juego y mecánicas. Fue anunciado en los premios Spike Video Game Awards en 2011.",
+    },
+    {
+        title: "Super Mario Maker 2",
+        img: "/images/games/9.webp",
+        price: "$39.99",
+        description:
+            "Super Mario Maker 2 es un videojuego de plataformas y de creación de niveles, desarrollado y publicado por Nintendo para Nintendo Switch. Es la secuela de Super Mario Maker y salió a la venta el 28 de junio de 2019 en todo el mundo.",
+    },
+    {
+        title: "Crash Bandicoot",
+        img: "/images/games/10.webp",
+        price: "$34.99",
+        description:
+            "Crash Bandicoot es el nombre de una serie de videojuegos protagonizado por el personaje del mismo nombre. Fue creada en 1996 por Naughty Dog, quien desarrolló los primeros cuatro títulos, bajo la distribución de Universal Interactive Studios.",
+    },
+    {
+        title: "Minecraft",
+        img: "/images/games/11.webp",
+        price: "$49.99",
+        description:
+            "Minecraft es un videojuego de construcción de tipo «mundo abierto» o en inglés sandbox creado originalmente por el sueco Markus Persson, que creo posteriormente Mojang Studios.",
+    },
+    {
+        title: "Cyberpunk 2077",
+        img: "/images/games/12.webp",
+        price: "$19.99",
+        description:
+            "Cyberpunk 2077 es un videojuego perteneciente al género rol de acción y disparos en primera persona desarrollado y publicado por CD Projekt, que se lanzó para Microsoft Windows, PlayStation 4 y Xbox One el 10 de diciembre de 2020, y posteriormente en PlayStation 5, Xbox Series X|S y Google Stadia.",
+    },
+    {
+        title: "FIFA 23",
+        img: "/images/games/13.webp",
+        price: "$44.99",
+        description:
+            "FIFA 23 es un videojuego de simulación de fútbol publicado por Electronic Arts. Es la trigésima entrega de la serie FIFA desarrollada por EA Sports, y la última entrega bajo el estandarte de FIFA.",
+    },
+    {
+        title: "Sonic Frontiers",
+        img: "/images/games/14.webp",
+        price: "$29.99",
+        description:
+            "Sonic Frontiers es un videojuego de plataformas de 2022 desarrollado por Sonic Team y publicado por Sega. Como Sonic, el jugador explora las islas Starfall para recolectar las Chaos Emeralds, después de que Sonic y sus amigos se separan al caer a través de un agujero de gusano.",
+    },
+    {
+        title: "Life Is Strange",
+        img: "/images/games/15.webp",
+        price: "$39.99",
+        description:
+            "Life Is Strange es una serie de juegos narrativos de aventuras publicados por External Studios de Square Enix. Creada por Dontnod Entertainment, la serie debutó con la primera entrega homónima, que se lanzó en cinco episodios a lo largo de 2015.",
+    },
+    {
+        title: "Control",
+        img: "/images/games/16.webp",
+        price: "$34.99",
+        description:
+            "Control es un videojuego de acción-aventura y disparos en tercera persona desarrollado por Remedy Entertainment y distribuido por 505 Games. Fue lanzado el 27 de agosto de 2019 en las plataformas Stadia, Microsoft Windows, PlayStation 4, Xbox One y Nintendo Switch.",
+    },
+];
